2fee6e2f2b643f68a8fd3141f694da0a72bb9501
Renie <renie.webdev@gmail.com>
2021-03-27 00:57:04 +0200

    - Server refactor - Removing etags - Trust proxy to fowarding

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ import config from './config'
 global.logger = logger
 global.ThrowError = error
 
-
 const defaultPort = config.APPPORT
 
 const logURLMappings = (expressInstance) => expressInstance._router.stack.
@@ -23,36 +22,27 @@ const logURLMappings = (expressInstance) => expressInstance._router.stack.
     map((r) => `${r.path} - ${r.stack[0].method} - ${r.stack[0].name}`).
     forEach((r) => logger.notice(r))
 
+const configureInstance = (expressInstance, corsOptions) => {
+    expressInstance.use(bodyParser.json())
+    expressInstance.use(helmet())
+    expressInstance.use(cookieParser())
+    expressInstance.use(cors(corsOptions))
 
-const getExpressInstance = (expressLib) => {
-    const instance = expressLib()
+    expressInstance.enable('trust proxy')
+    expressInstance.disable('etag')
 
-    const corsOptions = {
-        origin: config.FRONTENDADDRESS
-    }
-
-    instance.use(bodyParser.json())
-    instance.use(helmet())
-    instance.use(cookieParser())
-    instance.use(cors(corsOptions))
-    return instance
+    return expressInstance
 }
 
-export const startServer = (expressInstance, port = defaultPort) => {
+const getExpressInstance = () => configureInstance(express(), { origin: config.FRONTENDADDRESS })
+
+const startServer = (expressInstance, port = defaultPort) => {
     logURLMappings(expressInstance)
     const server = https.createServer(config.HTTPOPTIONS, expressInstance)
     server.listen(port, () => logger.info(`MGIntitute API Server listening at port ${port}...`))
     return server
 }
 
-const startApp = ({
-    expressLib,
-    setRouteFn,
-    port
-} = {
-    expressLib: express,
-    setRouteFn: setAllRoutes,
-    port: defaultPort
-}) => startServer(setRouteFn(getExpressInstance(expressLib)), port)
+const startApp = () => startServer(setAllRoutes(getExpressInstance()))
 
 module.exports = startApp
